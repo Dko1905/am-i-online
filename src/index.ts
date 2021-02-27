83b27a21434532cdc1f99a862cdb0ffb946dc569
Daniel Florescu <Daniel.florescu1905@protonmail.ch>
2021-02-27 09:12:53 +0100

    Now uses 4 different ping servers.

--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,77 @@
 import express, { Request, Response } from 'express'
-import {promise as ping} from 'ping'
+import { promise as ping } from 'ping'
 import db from 'croxydb'
 import Mustache from 'mustache'
-import {readFileSync} from 'fs'
+import { readFileSync } from 'fs'
 
 // Environment variables
 const PORT: number = Number.parseInt(process.env.PORT || "8080")
 const PING_INTERVAL: number = Number.parseInt(process.env.PING_INTERVAL || "1000")
-const DB_MAX_SIZE: number = 100000
+const PING_MIN_OUTAGE = 5 // Seconds it min must be down
 
 // Init variables
 const app = express()
 db.setAdapter('jsondb')
 
 if (db.get('pingResponseTime') == undefined) {
-	db.push('pingResponseTime', {start: 0, stop: 0})
+	db.push('pingResponseTime', { start: 0, stop: 1 })
 }
 
 // Read templates
-const rootTemplate = readFileSync('./static/index.html', {encoding: 'utf-8'})
-const pageTemplate = readFileSync('./static/page.html', {encoding: 'utf-8'})
+const rootTemplate = readFileSync('./static/index.html', { encoding: 'utf-8' })
+const pageTemplate = readFileSync('./static/page.html', { encoding: 'utf-8' })
 
 let start: number
 let lastTime: number | 'unknown' = 0
 setInterval(() => {
-	ping.probe("1.1.1.1").then((res) => {
-		if (res.time == 'unknown' && lastTime != 'unknown') {
-			start = Date.now()
-		} else if (lastTime == 'unknown' && res.time != 'unknown') {
-			const stop = Date.now()
-			if (Math.floor((stop - start) / 1000) < 5) {
-				console.info("Not pushing: drop is under 5 seconds")
-			} else {
-				console.info("pushing: ")
-				console.info({
-					start, stop
-				})
-				db.push("pingResponseTime", {
-					start,
-					stop
-				})
-			}
-		}
-		lastTime = res.time
+	const cloudflareDns = ping.probe("1.1.1.1", {
+		timeout: 1
 	})
-}, 1000)
+	const googleDns = ping.probe('8.8.8.8', {
+		timeout: 1
+	})
+	const googleUrl = ping.probe('google.com', {
+		timeout: 1
+	})
+	const openDns = ping.probe('208.67.222.123', {
+		timeout: 1
+	})
+	Promise.all([cloudflareDns, googleDns, googleUrl, openDns])
+		.then((responseArray) => {
+			// Go through all responses, save the timings
+			let responseTimeArr: number[] = []
+
+			for (const pingResponse of responseArray) {
+				if (pingResponse.time != 'unknown') {
+					responseTimeArr.push(pingResponse.time)
+				}
+			}
+			const responseTime = responseTimeArr.length > 0 ? responseTimeArr.reduce((prev, current) => prev + current ) / responseTimeArr.length : 'unknown'
+
+			if (responseTime == 'unknown' && lastTime != 'unknown') {
+				start = Date.now()
+			} else if (lastTime == 'unknown' && responseTime != 'unknown') {
+				const stop = Date.now()
+				if (Math.floor((stop - start)/ 1000) < PING_MIN_OUTAGE) {
+					console.info("Not pushing: drop is under 5 seconds")
+				} else {
+					console.info("Pushing:")
+					console.info({
+						start, stop
+					})
+					db.push("pingResponseTime", {
+						start,
+						stop
+					})
+				}
+			}
+			lastTime = responseTime
+		})
+}, PING_INTERVAL)
 
 app.get("/", (req: Request, res: Response) => {
 	const renderStart = Date.now()
-	const all: {start: number, stop: number}[] = db.get("pingResponseTime")
+	const all: { start: number, stop: number }[] = db.get("pingResponseTime")
 
 	let offline = 0
 	let offline_24h = 0
@@ -82,7 +105,7 @@ app.get("/page/:page", (req: Request, res: Response) => {
 	const renderStart = Date.now()
 
 	const page: number = Number.parseInt(req.params.page)
-	let all: {start: number, stop: number}[] = db.get("pingResponseTime")
+	let all: { start: number, stop: number }[] = db.get("pingResponseTime")
 
 	all = page == 1 ? all.slice(0, 9) : all.slice(((page - 1) * 10) - 1, ((page - 1) * 10) + 8)
 
